25c67105f791e7691050b63ceb3d814bc20e0e39
paul-writes-code <59710585+paul-writes-code@users.noreply.github.com>
2024-05-28 18:42:47 -0400

    Implement acceleration with drive and reverse gears
    - Cleanup player script
    - Remove mouse input
    - Cleanup input

--- objects/player.gd
+++ objects/player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody3D
 
+enum GEARS { DRIVE, REVERSE }
+
 @export_subgroup("Properties")
 @export var movement_speed = 5
 @export var jump_strength = 8
@@ -10,15 +12,14 @@ extends CharacterBody3D
 var weapon: Weapon
 var weapon_index := 0
 
-var mouse_sensitivity = 700
-var gamepad_sensitivity := 0.075
+var gears: Array[GEARS] = [ GEARS.DRIVE, GEARS.REVERSE ]
+var gear: GEARS
+var gear_index := 0
 
-var mouse_captured := true
+var gamepad_sensitivity := 0.075
 
 var movement_velocity: Vector3
 var rotation_target: Vector3
-
-var input_mouse: Vector2
 
 var health:int = 100
 var gravity := 0.0
@@ -47,8 +48,6 @@ signal health_updated
 
 func _ready():
 	
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-	
 	weapon = weapons[weapon_index] # Weapon must never be nil
 	initiate_change_weapon(weapon_index)
 
@@ -73,7 +72,7 @@ func _physics_process(delta):
 	
 	# Rotation
 	
-	camera.rotation.z = lerp_angle(camera.rotation.z, -input_mouse.x * 25 * delta, delta * 5)	
+	camera.rotation.z = lerp_angle(camera.rotation.z, 0, delta * 5)	
 	
 	camera.rotation.x = lerp_angle(camera.rotation.x, rotation_target.x, delta * 25)
 	rotation.y = lerp_angle(rotation.y, rotation_target.y, delta * 25)
@@ -103,67 +102,18 @@ func _physics_process(delta):
 	if position.y < -10:
 		get_tree().reload_current_scene()
 
-# Mouse movement
-
-func _input(event):
-	if event is InputEventMouseMotion and mouse_captured:
-		
-		input_mouse = event.relative / mouse_sensitivity
-		
-		rotation_target.y -= event.relative.x / mouse_sensitivity
-		rotation_target.x -= event.relative.y / mouse_sensitivity
-
+# Movement
 func handle_controls(_delta):
-	
-	# Mouse capture
-	
-	if Input.is_action_just_pressed("mouse_capture"):
-		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-		mouse_captured = true
-	
-	if Input.is_action_just_pressed("mouse_capture_exit"):
-		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-		mouse_captured = false
-		
-		input_mouse = Vector2.ZERO
-	
-	# Movement
-	
-	var input := Input.get_vector("move_left", "move_right", "drive_forward", "drive_back")
-	
-	movement_velocity = Vector3(input.x, 0, input.y).normalized() * movement_speed
-	
-	# Rotation
-	
-	var rotation_input := Input.get_vector("turn_right", "turn_left", "camera_down", "camera_up")
-	
-	rotation_target -= Vector3(-rotation_input.y, -rotation_input.x, 0).limit_length(1.0) * gamepad_sensitivity
-	rotation_target.x = clamp(rotation_target.x, deg_to_rad(-90), deg_to_rad(90))
-	
-	# Shooting
+	handle_acceleration()
+	handle_steering()
 	
 	action_shoot()
-	
-	# Jumping
-	
-	if Input.is_action_just_pressed("jump"):
-		
-		if jump_single or jump_double:
-			Audio.play("sounds/jump_a.ogg, sounds/jump_b.ogg, sounds/jump_c.ogg")
-		
-		if jump_double:
-			
-			gravity = -jump_strength
-			jump_double = false
-			
-		if(jump_single): action_jump()
-		
-	# Weapon switching
+	action_jump()
 	
 	action_weapon_toggle()
+	action_switch_gears()
 
 # Handle gravity
-
 func handle_gravity(delta):
 	
 	gravity += 20 * delta
@@ -173,17 +123,37 @@ func handle_gravity(delta):
 		jump_single = true
 		gravity = 0
 
-# Jumping
+# Driving
+func handle_acceleration():
+	var acceleration := Input.get_action_strength("accelerate")
+	var deceleration := Input.get_action_strength("brake")
+	var movement_z = -acceleration
+	
+	if gear == GEARS.REVERSE: 
+		movement_z *= -1
+	
+	movement_velocity = Vector3(0, 0, movement_z).limit_length(1.0) * movement_speed
 
+func handle_steering():
+	var steering_input := Input.get_axis("steer_left", "steer_right")
+	rotation_target -= Vector3(0, steering_input, 0).limit_length(1.0) * gamepad_sensitivity
+	rotation_target.x = clamp(rotation_target.x, deg_to_rad(-90), deg_to_rad(90))
+
+# Jumping
 func action_jump():
-	
-	gravity = -jump_strength
-	
-	jump_single = false;
-	jump_double = true;
+	if Input.is_action_just_pressed("jump"):
+		if jump_single or jump_double:
+			gravity = -jump_strength
+			Audio.play("sounds/jump_a.ogg, sounds/jump_b.ogg, sounds/jump_c.ogg")
+		
+		if jump_double: 
+			jump_double = false
+			
+		if jump_single: 
+			jump_single = false;
+			jump_double = true;
 
 # Shooting
-
 func action_shoot():
 	
 	if Input.is_action_pressed("shoot"):
@@ -236,21 +206,21 @@ func action_shoot():
 			impact_instance.position = raycast.get_collision_point() + (raycast.get_collision_normal() / 10)
 			impact_instance.look_at(camera.global_transform.origin, Vector3.UP, true) 
 
-# Toggle between available weapons (listed in 'weapons')
+# Switch gears
+func action_switch_gears():
+	if Input.is_action_just_pressed("switch_gears"):
+		gear_index = wrap(gear_index + 1, 0, gears.size())
+		gear = gears[gear_index]
 
+# Toggle between available weapons (listed in 'weapons')
 func action_weapon_toggle():
-	
 	if Input.is_action_just_pressed("weapon_toggle"):
-		
 		weapon_index = wrap(weapon_index + 1, 0, weapons.size())
 		initiate_change_weapon(weapon_index)
-		
 		Audio.play("sounds/weapon_change.ogg")
 
 # Initiates the weapon changing animation (tween)
-
 func initiate_change_weapon(index):
-	
 	weapon_index = index
 	
 	tween = get_tree().create_tween()
@@ -259,9 +229,7 @@ func initiate_change_weapon(index):
 	tween.tween_callback(change_weapon) # Changes the model
 
 # Switches the weapon model (off-screen)
-
 func change_weapon():
-	
 	weapon = weapons[weapon_index]
 
 	# Step 1. Remove previous weapon model(s) from container
@@ -288,7 +256,6 @@ func change_weapon():
 	crosshair.texture = weapon.crosshair
 
 func damage(amount):
-	
 	health -= amount
 	health_updated.emit(health) # Update health on HUD
 	
